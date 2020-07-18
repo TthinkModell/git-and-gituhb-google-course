21b1043d646e4edfb2d46fbb2697bb150cbb0ef2
ValeriChakarov <valeritchakarov@gmail.com>
2020-07-18 19:17:41 +0100

    Adding changes without git add command

--- src/DiffingFiles/File1.java
+++ src/DiffingFiles/File1.java
@@ -1,11 +1,16 @@
 package DiffingFiles;
 
+
 public class File1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("Changes without git add!");
     }
     //blablabab
     /// Some more changes
     /// Some more changes
+
+
 
 }
