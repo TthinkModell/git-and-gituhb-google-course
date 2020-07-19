61741105c07e3e1856621ac3a92166a930728266
ValeriChakarov <valeritchakarov@gmail.com>
2020-07-19 16:13:50 +0100

    File1 Cleanup

---
+++ src/DiffingFiles/File1.java
@@ -0,0 +1,10 @@
+package DiffingFiles;
+
+
+public class File1 {
+public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("Changes without git add!");
+        }
+}
